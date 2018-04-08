6b81fb6f6fb652836abb67fca73c6086fee56d83
dalidog <league@iMac.attlocal.net>
2018-04-08 11:31:21 -0700

    hangdd

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,8 @@
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -14,11 +16,15 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Hangman implements ActionListener{
+public class Hangman implements KeyListener {
 	ArrayList<String> words = new ArrayList<String>();
-	Stack s = new Stack();
-String wordtosolve;
-String mysteryword;
+	Stack<String> s = new Stack<String>();
+	String wordtosolve;
+	String mysteryword;
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+int lives = 6;
 	public static void main(String[] args) throws FileNotFoundException {
 		Hangman h = new Hangman();
 		h.getwords();
@@ -60,22 +66,22 @@ String mysteryword;
 	}
 
 	public void makelines() {
-		 wordtosolve = (String) s.pop();
-		 System.out.println(wordtosolve);
+		wordtosolve = (String) s.pop();
+		System.out.println(wordtosolve);
 		mysteryword = "";
-		for (int i = 0; i < ((String) s.pop()).length(); i++) {
+		for (int i = 0; i < wordtosolve.length(); i++) {
 			mysteryword += "_ ";
 		}
-		JFrame frame = new JFrame("HaNgMaN");
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel(mysteryword);
+		frame = new JFrame("Hangman!!! PRESS ENTER WHEN YOU HAVE GUESSED ALL OF THE LETTERS!");
+		panel = new JPanel();
+		label = new JLabel(mysteryword);
 		label.setFont(new Font("Serif", Font.PLAIN, 50));
 		frame.add(panel);
 		panel.add(label);
+		frame.addKeyListener(this);
 		frame.setVisible(true);
 		frame.pack();
-		
-		
+
 	}
 
 	public void changelines() {
@@ -83,18 +89,50 @@ String mysteryword;
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getKeyCode() == 0) {
+
+			wordtosolve = s.pop();
+			mysteryword = "";
+			lives = 6;
+			for (int i = 0; i < wordtosolve.length(); i++) {
+				mysteryword += "_ ";
+			}
+			label.setText(mysteryword);
+			// TODO Auto-generated method stub
+		}
+		// TODO Auto-generated method stub
 		String tempString = "";
+
 		// TODO Auto-generated method stub
 		for (int i = 0; i < wordtosolve.length(); i++) {
-			if(e.getSource().toString().equals(wordtosolve.charAt(i)+"")) {
-				tempString+=wordtosolve.charAt(i)+" ";
-			}
-			else {
-				tempString+=mysteryword.charAt(i*2)+" ";
+			if (e.getKeyChar() == (wordtosolve.charAt(i))) {
+				tempString += wordtosolve.charAt(i) + " ";
+
+			} else {
+				tempString += mysteryword.charAt(i * 2) + " ";
+lives = lives-1;
+if(lives == 0) {
+	JOptionPane.showMessageDialog(null, "YOU SUCKKKKKKKK! You lost");
+}
 			}
 		}
-		mysteryword=tempString;
-		}
+		mysteryword = tempString;
+		label.setText(tempString);
 		
+
+
+	}
+
 }
